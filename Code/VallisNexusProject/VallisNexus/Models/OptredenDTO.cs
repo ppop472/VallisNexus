b78841bcd8f1c19b597a886e2e9215265ff6ed1a
Christian <kakcsi02@gmail.com>
2026-07-02 10:25:15 +0200

    DTO-comment-gezet
    DTO-comment-gezet

--- Code/VallisNexusProject/VallisNexus/Models/OptredenDTO.cs
+++ Code/VallisNexusProject/VallisNexus/Models/OptredenDTO.cs
@@ -8,6 +8,8 @@ namespace VallisNexus.Models
 {
     public class OptredenDTO : Optreden
     {
+
+        // Deze is toegevoegd omdat ik bij het maken heel logisch vond om artiestnaam en podiumnaam ook erbij te zetten, een DTO is dus hiervoor goed. Vind ik.
         public string artiestNaam { get; private set; }
         public int teller { get; private set; }
         public string podiumNaam { get; private set; }
